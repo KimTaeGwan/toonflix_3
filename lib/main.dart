de3f3782a1f70f1bec62885751335c601fc0f438
KimTaeGwan <chtaeryu@gmail.com>
2023-04-21 13:42:56 +0900

    save

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:toonflix_3/screens/home_screen.dart';
+import 'package:toonflix_3/services/api_service.dart';
 
 void main() {
+  ApiService().getTodaysToons();
   runApp(const App());
 }
 
