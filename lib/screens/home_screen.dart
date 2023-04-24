a45111d87f918733ff24ce7fa4bb227c1e3596b5
KimTaeGwan <chtaeryu@gmail.com>
2023-04-24 16:48:54 +0900

    save

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:toonflix_3/models/webtoon_model.dart';
+import 'package:toonflix_3/services/api_service.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -12,8 +13,23 @@ class _HomeScreenState extends State<HomeScreen> {
   List<WebtoonModel> webtoons = [];
   bool isLoading = true;
 
+  void waitForWebtoon() async {
+    webtoons = await ApiService.getTodaysToons();
+    isLoading = false;
+    setState(() {});
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    waitForWebtoon();
+  }
+
   @override
   Widget build(BuildContext context) {
+    print(webtoons);
+    print(isLoading);
+
     return Scaffold(
       backgroundColor: Colors.white,
       appBar: AppBar(
